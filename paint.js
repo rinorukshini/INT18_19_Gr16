a5bd757a0c577eebfb2be56e119254aec4fc68a2
Rina Bunjaku <bunjaku00@hotmail.com>
2019-01-01 15:41:45 +0100

    try

--- paint.js
+++ paint.js
@@ -1,87 +1,72 @@
-var started = false;
-var canvas, context;
-var stampId = '';
-var lastColor = 'black';
-var lastStampId = '';
+//Github: https://github.com/mcnitt/simple-jquery-drawing-app
 
-function init() {
-	canvas = $('#imageView').get(0);
-	context = canvas.getContext('2d');
-	
-	// Auto-adjust canvas size to fit window.
-	canvas.width  = window.innerWidth - 75;
-	canvas.height = window.innerHeight - 75;
-  
-	//$('#container').get(0).addEventListener('mousemove', onMouseMove, false);
-	
-	canvas.addEventListener('click', onClick, false);
-	canvas.addEventListener('mousemove', onMouseMove, false);
-	// Add events for toolbar buttons.
-	$('#red').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
-	$('#pink').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
-	$('#fuchsia').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
-	$('#orange').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
-	$('#yellow').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
-	$('#lime').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
-	$('#green').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
-	$('#blue').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
-	$('#purple').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
-	$('#black').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
-	$('#white').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
+var color = $(".selected").css("background-color");
+var $canvas = $("canvas");
+//Select the first, only canvas element. Select the actual HTML element using the array syntax [index], get the 2d context.
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
+
+//When clicking control list items
+$(".controls").on("click", "li", function() {
+    //deselect sibling elements
+    $(this).siblings().removeClass("selected");
+    //select clicked element
+    $(this).addClass("selected");
+    //cache current color here
+    color = $(".selected").css("background-color");
+    console.log("selected color = " + color);
+});
+
+//When "New Color" is pressed
+$("#revealColorSelect").click(function() {
+    //show or hide color select
+    changeColor();
+    $("#colorSelect").toggle();
+});
+
+//update the new color span
+function changeColor() {
+    var r = $("#red").val();
+    var g = $("#green").val();
+    var b = $("#blue").val();
+    var a = $("#alpha").val() / 100;
+
+    $("#newColor").css("background-color", "rgba(" + r + "," + g + "," + b + "," + a + ")");
+    console.log($("#newColor").css("background-color"));
+
 }
 
-function onMouseMove(ev) {
-	var x, y;
-		
-	
-	if (ev.layerX >= 0) {
-		
-		x = ev.layerX - 5;
-		y = ev.layerY - 5;
-	}
-	else if (ev.offsetX >= 0) {
-		
-		x = ev.offsetX - 5;
-		y = ev.offsetY - 5;
-	}
-	
-	if (!started) {
-		started = true;
+//When color sliders change
+$("input").change(changeColor);
 
-		context.beginPath();
-		context.moveTo(x, y);		
-	}
-	else {
-		context.lineTo(x, y);
-		context.stroke();
-	}
-	
-	$('#stats').text(x + ', ' + y);
-}
+//When "Add Color" is pressed
+$("#addNewColor").click(function() {
+    //append the color to the controls ul
+    var $newColor = $("<li></li>");
+    $newColor.css("background-color", $("#newColor").css("background-color"));
+    $(".controls ul").append($newColor);
+    //select the new color
+    $newColor.click();
+});
 
-function onClick(e) {
-	if (stampId.length > 0) {
-		context.drawImage($(stampId).get(0), e.pageX - 90, e.pageY - 60, 80, 80);
-	}
-}
+//On mouse events on the canvas
+$canvas.mousedown(function(e) {
+    lastEvent = e;
+    mouseDown = true;
 
-function onColorClick(color) {
-	// Start a new path to begin drawing in a new color.
-	context.closePath();
-	context.beginPath();
-	
-	// Select the new color.
-	context.strokeStyle = color;
-	
-	// Highlight selected color.
-	var borderColor = 'white';
-	if (color == 'white' || color == 'yellow') {
-		borderColor = 'black';
-	}
-	
-	$('#' + lastColor).css("border", "0px dashed white");
-	$('#' + color).css("border", "2px dashed " + borderColor);
-	
-	// Store color so we can un-highlight it next time around.
-	lastColor = color;
-}
+}).mousemove(function(e) {
+    if (mouseDown) {
+        //Draw lines
+        context.beginPath();
+        context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+        context.lineTo(e.offsetX, e.offsetY);
+        context.strokeStyle = color;
+        context.stroke();
+        lastEvent = e;
+    }
+}).mouseup(function() {
+    mouseDown = false;
+}).mouseleave(function() {
+    $canvas.mouseup();
+});
