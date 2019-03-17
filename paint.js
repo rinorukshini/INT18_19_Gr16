4e77207d4399fba25b3390afefd44b61b9fcc90f
Redon Osmanollaj <redon_osmanollaj@hotmail.com>
2019-03-17 22:54:18 +0100

    Ndryshime finale

--- paint.js
+++ paint.js
@@ -1,31 +1,31 @@
-//Github: https://github.com/mcnitt/simple-jquery-drawing-app
+
 
 var color = $(".selected").css("background-color");
 var $canvas = $("canvas");
-//Select the first, only canvas element. Select the actual HTML element using the array syntax [index], get the 2d context.
+
 var context = $canvas[0].getContext("2d");
 var lastEvent;
 var mouseDown = false;
 
-//When clicking control list items
+
 $(".controls").on("click", "li", function() {
-    //deselect sibling elements
+    
     $(this).siblings().removeClass("selected");
-    //select clicked element
+    
     $(this).addClass("selected");
-    //cache current color here
+    
     color = $(".selected").css("background-color");
     console.log("selected color = " + color);
 });
 
-//When "New Color" is pressed
+
 $("#revealColorSelect").click(function() {
-    //show or hide color select
+    
     changeColor();
     $("#colorSelect").toggle();
 });
 
-//update the new color span
+
 function changeColor() {
     var r = $("#red").val();
     var g = $("#green").val();
@@ -37,20 +37,20 @@ function changeColor() {
 
 }
 
-//When color sliders change
+
 $("input").change(changeColor);
 
-//When "Add Color" is pressed
+
 $("#addNewColor").click(function() {
-    //append the color to the controls ul
+    
     var $newColor = $("<li></li>");
     $newColor.css("background-color", $("#newColor").css("background-color"));
     $(".controls ul").append($newColor);
-    //select the new color
+    
     $newColor.click();
 });
 
-//On mouse events on the canvas
+
 $canvas.mousedown(function(e) {
     lastEvent = e;
     mouseDown = true;
